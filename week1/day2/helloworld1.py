58db6e4990f284ae80f77804a8bb04be749dfeab
CsilvaD99 <Carlos.silva.clss17@gmail.com>
2022-06-07 14:55:47 -0500

    Day 2

---
+++ week1/day2/helloworld1.py
@@ -0,0 +1,53 @@
+from asyncio.base_subprocess import BaseSubprocessTransport
+from turtle import clear
+from unicodedata import decimal
+
+
+def sayHi():
+    Print("Hi")
+
+def sayHi():
+    print(this is the body")
+
+boolean is true or false
+True is it truthy
+False is it falsy
+
+teacher = "joe"
+
+always name the function something clear
+
+#integers
+number = 1
+
+#float
+any numbers with a decimal
+#be careful woth floats because of e like the calculator
+
+#list in pyhton or array on javascript
+# is anthing in [] it can contain multiple types of data
+#you acces a list by using what is called the index of the list
+#index of the list starts at 0 
+students = ["carlos","juan","brandon"]
+print(student[2]) that is brandon- 0,1,2. 
+#list are extremely important
+
+#dictionary on python or Objact in js
+variable = {} it uses squigly 
+#it shuold have  a "key": "value" the value does not have to be a stirng
+#a value can be a string an intiger a boolean a float a list a variable
+#a dictionary can be massive 100 200 and navigating them is important
+# "key": "value"
+# example "number":"1", "hot":"true". print
+
+data = {
+    "president": {
+        "name": "zaphod beeblebrox",
+        "species" "betelgeusian",
+        "location"" ["south america"]
+    }
+}
+
+print(data["president"]["name"])
+print(data["president"]["location"][0])
+
---
+++ week1/day2/loops.py
@@ -0,0 +1,69 @@
+#loops
+#loops are used to go through a specific amount of times and execute code every time it goes through it 
+
+#It iterates over something 
+
+#it iterates over list
+
+#it can iterate over dicctionaries
+
+#it can call something a bunch of times
+
+#It can break you computer immediatly forever and you will have to restart comp
+
+#while loop does something while this is true 2>1 is an example#
+
+#while (condition to evaluate) while 2>1: indent print("yay")
+
+#condition can be checked many times
+
+#to  prevent infinite loop you have to make it stop after a few times
+
+#example
+#   num = 0
+# while 2 > num:
+#   print ("yay")
+#   num+=1 which adds one to the number this loop prints 2 yays
+#   num+=2 would have one yay
+
+#len gives you the length of the list
+
+#for - it's a type of loop
+
+#ftuit = ("apple","pineapple","bannana") emojis are a string
+
+#for  singular in list - "for" a single item of this list
+#for fruit in fruits:
+#   Print(fruit)  - this list apple then does a loop and then pineapple, the bannana but then it stops beacuse that is how long the list is 
+
+# "for" most common used its more efficient
+
+#you can 'pass' if you want to skip something 
+#'else' can be used to list the res of the list
+
+#name = input('What is you name?')
+#print('Hello '+ name + '!')
+
+#you can use the input function it waits for you to inputsomething 
+
+#Name = input("what is your name?")
+#Subject = input("what is your favorite subject?")
+#print ((( Name +"'"+"s ") + "favorite subject in school is ")+ Subject +".")
+
+#import random
+#MyRandomNumber = (random.randint(1,10))
+
+
+
+run = True 
+while run:
+    userInput = int(input('enter number'))
+    if userInput == 7
+        print('winner')
+        run = False
+    else:
+        print('try again!')
+        continue
+
+
+
