99a50b1ba59bfe29d77e534c1e7317375fe01936
CsilvaD99 <Carlos.silva.clss17@gmail.com>
2022-06-30 10:56:55 -0500

    week4 da4

---
+++ week4/blackjack-exercise-main/scripts/main.js
@@ -0,0 +1,194 @@
+const DealerHand = document.getElementById("dealer-hand")
+const PlayerHand = document.getElementById("player-name")
+
+
+window.addEventListener('DOMContentLoaded', function() {
+  // Execute after page load
+})
+//card    1-4=Ace/ 5-8=#2/ 9-12=#3/ 13-16=#4 /17-20=#5/ 21-24=#6/ 
+//        25-28=#7/ 29-32=#8/ 33-36=#9/ 37-40=#10/ 41-44=#J/ 45-48=#Q/ 49-52=#K
+const deck = {
+card1: {
+  FaceValue:
+  CardSuit: 
+  CardImage:
+  Points:
+
+}
+card2: {
+  FaceValue:
+  CardSuit: 
+  CardImage:
+  Points:
+
+}
+card3: {
+  FaceValue:
+  CardSuit: 
+  CardImage:
+  Points:
+
+}
+card4: {
+  FaceValue:
+  CardSuit: 
+  CardImage:
+  Points:
+
+}
+card5: {
+  FaceValue:
+  CardSuit: 
+  CardImage:
+  Points:
+
+}
+card6: {
+  FaceValue:
+  CardSuit: 
+  CardImage:
+  Points:
+
+}
+card7: {
+  FaceValue:
+  CardSuit: 
+  CardImage:
+  Points:
+
+}
+card8: {
+  FaceValue:
+  CardSuit: 
+  CardImage:
+  Points:
+
+}
+card9: {
+  FaceValue:
+  CardSuit: 
+  CardImage:
+  Points:
+
+}
+card10: {
+  FaceValue:
+  CardSuit: 
+  CardImage:
+  Points:
+
+}
+card: {
+  FaceValue:
+  CardSuit: 
+  CardImage:
+  Points:
+
+}
+card: {
+  FaceValue:
+  CardSuit: 
+  CardImage:
+  Points:
+
+}
+card: {
+  FaceValue:
+  CardSuit: 
+  CardImage:
+  Points:
+
+}
+card: {
+  FaceValue:
+  CardSuit: 
+  CardImage:
+  Points:
+
+}
+card: {
+  FaceValue:
+  CardSuit: 
+  CardImage:
+  Points:
+
+}
+card: {
+  FaceValue:
+  CardSuit: 
+  CardImage:
+  Points:
+
+}
+card: {
+  FaceValue:
+  CardSuit: 
+  CardImage:
+  Points:
+
+}
+card: {
+  FaceValue:
+  CardSuit: 
+  CardImage:
+  Points:
+
+}
+card: {
+  FaceValue:
+  CardSuit: 
+  CardImage:
+  Points:
+
+}
+card: {
+  FaceValue:
+  CardSuit: 
+  CardImage:
+  Points:
+
+}
+card: {
+  FaceValue:
+  CardSuit: 
+  CardImage:
+  Points:
+
+}
+card: {
+  FaceValue:
+  CardSuit: 
+  CardImage:
+  Points:
+
+}
+card: {
+  FaceValue:
+  CardSuit: 
+  CardImage:
+  Points:
+
+}
+card: {
+  FaceValue:
+  CardSuit: 
+  CardImage:
+  Points:
+
+}
+card: {
+  FaceValue:
+  CardSuit: 
+  CardImage:
+  Points:
+
+}
+card: {
+  FaceValue:
+  CardSuit: 
+  CardImage:
+  Points:
+
+}
+
+}
